610e55d4f29c4b2d6a074ea0cedfb6a4f24921ca
BK1918houxiaoqin <1825425238@qq.com>
2019-11-12 07:36:46 +0800

    首页和tabBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app">
-  <router-view></router-view>
-  
+    <router-view></router-view>
+    <tabBar />
   </div>
  
 </template>
 
 <script>
-
+import tabBar from "./components/tabBar"
 export default {
   name: 'App',
+  components:{
+    tabBar,
+  },
 created(){
   console.log(this,this.$route);
 },
