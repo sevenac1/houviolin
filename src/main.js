1737f09e100d56341af48acc125db13b75f82c0c
beike1918caojingjing <1559274817@qq.com>
2019-11-14 20:53:58 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import VueTouch from "vue-touch"
 import { Lazyload } from 'vant';
 
@@ -10,14 +9,7 @@ Vue.use(Lazyload);
 Vue.use(VueTouch,{name:"v-touch"});
 import "@common/components"
 Vue.use(VueTouch,{name:"v-touch"})
-=======
 
-
->>>>>>> 3ba5d35e57460cb77e672a73b2c222d638e60330
-=======
-
-
->>>>>>> 3ba5d35e57460cb77e672a73b2c222d638e60330
 new Vue({
   router,
   VueTouch,
