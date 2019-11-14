ed53bc649fa402379a5a2c77c852534e51770c9b
BK1918houxiaoqin <1825425238@qq.com>
2019-11-14 20:14:39 +0800

    xiugai

--- src/router/auction/index.js
+++ src/router/auction/index.js
@@ -2,18 +2,13 @@ export default {
     path:"/auction",
     component:_=>import("@page/auction"),
     name:"auction",
-<<<<<<< HEAD
     meta:{
         flag:true,
         requiredAuth:false,
-    }
-}
-=======
-   
+    } 
 
 }
     
 
 
 
->>>>>>> 8a59fa3749ebc4b5e89f821097702f2defb72a4c
